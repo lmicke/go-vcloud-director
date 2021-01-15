3a56ee0839b52480b8dd0c864bfc630a7b5701a3
Lukas <LukasValentin.Micke@bertelsmann.de>
2021-01-15 12:17:58 +0100

    Added changing to Default Deny Try 1

--- govcd/vdc_df.go
+++ govcd/vdc_df.go
@@ -1,8 +1,10 @@
 package govcd
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -14,6 +16,7 @@ import (
 type DFW struct {
 	Section *DFWSection
 	Client  *Client
+	Etag    string
 }
 
 func NewDFW(cli *Client) *DFW {
@@ -72,6 +75,9 @@ func (dfw *DFW) CheckDistributedFirewall(VdcID string) (bool, error) {
 		return true, nil
 	}
 
+	header := resp.Header
+	dfw.Etag = header.Get("ETag")
+
 	return false, fmt.Errorf("Unexptected Status Code %s", resp.Status)
 }
 
@@ -90,6 +96,31 @@ func (dfw *DFW) DeleteDistributedFirewall(VdcID string) error {
 		return err
 	}
 	if resp.StatusCode != 204 {
+		return fmt.Errorf("Deleting Firewall was not successfull, API Response is:  %s", resp.Status)
+	}
+	return nil
+}
+
+func (dfw *DFW) UpdateDistributedFirewall(VdcID string) error {
+	base := dfw.Client.VCDHREF
+	ID := urnToUID(VdcID)
+	add, err := url.Parse(types.DFWRequest + ID)
+	if err != nil {
+		return fmt.Errorf("Error building url for DFW Delete: %s", err)
+	}
+	dfwURL := base.ResolveReference(add)
+	log.Printf("[DEBUG] Update Distributed Firewall URL is: %s", dfwURL.String())
+	log.Printf("[DEBUG] Etag is: %s", dfw.Etag)
+	// Build default Change
+	dfw.Section.Rules[0].Name = "Default Deny"
+	dfw.Section.Rules[0].Action = "deny"
+
+	resp, err := dfw.Client.ExecuteRequestWithCustomHeader(dfwURL.String(), http.MethodPut, "", "error reaching dfwURL: %s", dfw.Etag, dfw.Section, dfw.Section)
+	log.Printf("[DEBUG] Response for Update  Firewall: %v", resp)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
 		return fmt.Errorf("Deleting Firewall was not successfull, API Response is:  %s", resp.Status)
 	}
 	return nil
@@ -131,4 +162,60 @@ type DFWApplied struct {
 
 type DFWAppliedTo struct {
 	ID DFWApplied `xml:"appliedTo"`
+}
+
+// Update Request with Custom Etag Header:
+
+func (cli *Client) NewRequestWithCustomHeader(params map[string]string, notEncodedParams map[string]string, method string, reqUrl url.URL, body io.Reader, etag string) *http.Request {
+	var headers http.Header
+	headers.Add("If-Match", etag)
+	return cli.newRequest(params, nil, method, reqUrl, body, cli.APIVersion, headers)
+
+}
+
+func (cli *Client) ExecuteRequestWithCustomHeader(pathURL, requestType, contentType, errorMessage, etag string, payload, out interface{}) (*http.Response, error) {
+	if !isMessageWithPlaceHolder(errorMessage) {
+		return &http.Response{}, fmt.Errorf("error message has to include place holder for error")
+	}
+
+	url, _ := url.ParseRequestURI(pathURL)
+
+	var req *http.Request
+	switch requestType {
+	case http.MethodPost, http.MethodPut:
+
+		marshaledXml, err := xml.MarshalIndent(payload, "  ", "    ")
+		if err != nil {
+			return &http.Response{}, fmt.Errorf("error marshalling xml data %s", err)
+		}
+		body := bytes.NewBufferString(xml.Header + string(marshaledXml))
+
+		req = cli.NewRequestWithCustomHeader(map[string]string{}, nil, requestType, *url, body, etag)
+
+	default:
+		req = cli.NewRequestWithCustomHeader(map[string]string{}, nil, requestType, *url, nil, etag)
+	}
+
+	if contentType != "" {
+		req.Header.Add("Content-Type", contentType)
+	}
+
+	setHttpUserAgent(cli.UserAgent, req)
+
+	resp, err := cli.Http.Do(req)
+	if err != nil {
+		return resp, fmt.Errorf(errorMessage, err)
+	}
+
+	if err = decodeBody(types.BodyTypeXML, resp, out); err != nil {
+		return resp, fmt.Errorf("error decoding response: %s", err)
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return resp, fmt.Errorf("error closing response body: %s", err)
+	}
+
+	// The request was successful
+	return resp, nil
 }
